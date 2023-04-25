5c9b2377e3106e9413551d14fc8417b8104dcda6
Sreeginy <sreeginy98@gmail.com>
2023-04-25 16:34:07 +0530

    update

--- src/main/java/com/system/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/system/config/JwtAuthenticationEntryPoint.java
@@ -15,6 +15,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 
 	private static final long serialVersionUID = -7858869558953243875L;
 
+
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
