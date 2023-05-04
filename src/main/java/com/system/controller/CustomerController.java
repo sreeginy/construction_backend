1ed8805446e29f6e042fb0fb96af1fe87d3279d0
Sreeginy <sreeginy98@gmail.com>
2023-05-04 18:29:28 +0530

    update

--- src/main/java/com/system/controller/CustomerController.java
+++ src/main/java/com/system/controller/CustomerController.java
@@ -124,7 +124,6 @@ public class CustomerController {
 //        return CommonResponse.generateResponse(null,1000,"Deleted Successfully!!!");
 //    }
 
-
     //Delete customer
     @GetMapping(path = "/delete/{id}")
     public @ResponseBody CommonResponse deleteCustomerById(@PathVariable("id") Integer customerId){
