0a3bdda79134bb05eb9f85bf8f8f6e669222ba85
Sreeginy <sreeginy98@gmail.com>
2023-05-15 18:54:01 +0530

    update

--- src/main/java/com/system/controller/OrderController.java
+++ src/main/java/com/system/controller/OrderController.java
@@ -16,7 +16,7 @@ public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
-    
+
     //Add Orders
     @PostMapping(path = "/add")
     public @ResponseBody CommonResponse createOrder(@RequestBody Orders orders){
